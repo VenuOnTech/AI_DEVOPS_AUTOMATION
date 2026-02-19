035290a5cb8ae9d180a7a5e1f4441a886d91499e
B.R.M.PHANI DEEPAK <deepak.24bcb7227@vitapstudent.ac.in>
2026-02-19 12:13:11 +0530

    Create risk_model.py

---
+++ ai/risk_model.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import joblib
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+MODEL_PATH = "ai/deployment_model.pkl"
+DATA_PATH = "ai/deployment_history.csv"
+
+# If model doesn't exist yet → allow deployment
+if not os.path.exists(MODEL_PATH):
+    print(0.0)
+    exit()
+
+model = joblib.load(MODEL_PATH)
+
+try:
+    lines_changed = int(sys.argv[1])
+    files_changed = int(sys.argv[2])
+    commit_message_length = int(sys.argv[3])
+    churn_rate = float(sys.argv[4])
+except:
+    print(0.0)
+    exit()
+
+commit_hour = datetime.now().hour
+previous_failure_rate = 0
+
+if os.path.exists(DATA_PATH):
+    data = pd.read_csv(DATA_PATH)
+    if len(data) > 0:
+        previous_failure_rate = data["failed"].mean()
+
+features = np.array([[
+    lines_changed,
+    files_changed,
+    commit_message_length,
+    commit_hour,
+    churn_rate,
+    previous_failure_rate
+]])
+
+probability = model.predict_proba(features)[0][1]
+
+print(round(float(probability), 4))
